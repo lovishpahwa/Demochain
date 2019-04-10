b877af43057006b675d99dbec779f33f7650fc77
Lovish Pahwa <pahwa.lovish95@gmail.com>
2019-04-10 17:34:11 +0530

    Comment added

--- Demochain/src/demochain/DemoChain.java
+++ Demochain/src/demochain/DemoChain.java
@@ -1,5 +1,6 @@
 /**
  * https://medium.com/programmers-blockchain/create-simple-blockchain-java-tutorial-from-scratch-6eeed3cb03fa
+ * https://medium.com/programmers-blockchain/creating-your-first-blockchain-with-java-part-2-transactions-2cdac335e0ce
  */
 package demochain;
 
